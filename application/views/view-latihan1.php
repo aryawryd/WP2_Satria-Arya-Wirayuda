f1c4d53ae5d5399ce0a41d758574879981f6e9c4
aryawryd <aryawirayuda09@gmail.com>
2023-03-30 16:02:05 +0700

    Edit VIEWS

--- application/views/view-latihan1.php
+++ application/views/view-latihan1.php
@@ -9,7 +9,6 @@
     Nilai 1 = <?= $nilai1; ?>
     Nilai 2 = <?= $nilai2; ?>
     ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
-    Tes
 </body>
 
 </html>
